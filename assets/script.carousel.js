e080faa9d7390c899a4c47ef252a3dce713ef737
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-09-09 08:58:46 +0900

    Product banner static part is done

---
+++ assets/script.carousel.js
@@ -0,0 +1,12 @@
+console.log("Bismillah")
+
+window.addEventListener('load', function() {
+    const swiper = new Swiper(".swiper--home-collection", {
+        slidesPerView: 4,
+            scrollbar: {
+              el: ".swiper-scrollbar",
+              hide: true,
+            },
+          });
+          
+       })
