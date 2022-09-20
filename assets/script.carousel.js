4fea2fc5ccca6985e6cf684787383c4c2c1d200a
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-09-21 01:13:46 +0900

    pdp header some part is done

--- assets/script.carousel.js
+++ assets/script.carousel.js
@@ -1,12 +1,24 @@
 console.log("Bismillah")
-
 window.addEventListener('load', function() {
-    const swiper = new Swiper(".swiper--home-collection", {
-        slidesPerView: 4,
-            scrollbar: {
-              el: ".swiper-scrollbar",
-              hide: true,
-            },
-          });
-          
+  const featuredCollectionCarousel=document.querySelector(".swiper--home-collection");
+  const pdpHeaderCarousel=document.querySelector(".swiper--pdp");
+if(featuredCollectionCarousel){
+  const swiper = new Swiper(".swiper--home-collection", {
+      slidesPerView: 4,
+          scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+          },
+        });
+
+}
+if(pdpHeaderCarousel){
+  const swiperPdpHeader = new Swiper(".swiper--pdp", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });     
+
+}
        })
